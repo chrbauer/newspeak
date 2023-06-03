281bab84028c6ad2c256d93fc15de6cca043bbde
Christoph Bauer <mail@christoph-bauer.net>
2023-06-03 14:58:32 +0200

    Scope at compiling

--- bootstrap/app/Newspeak/Compile.hs
+++ bootstrap/app/Newspeak/Compile.hs
@@ -3,6 +3,8 @@
 
 module Newspeak.Compile where
 
+import Control.Monad
+import Control.Monad.State.Strict
 import Newspeak.AST
 import Language.Wasm.Structure
 import Language.Wasm.Builder
@@ -10,37 +12,50 @@ import Newspeak.AST
 import Data.Proxy
 import Newspeak.AST (AST(MathExpr, FunDecl))
 import qualified Data.Text.Lazy as TL
+import qualified Data.Map as M
 
+
+data Scope = Scope {
+  scopeParams :: M.Map String (Loc I32),
+  scopeLocals :: M.Map String (Loc I32),
+  scopeFuncs :: M.Map String (Loc I32)
+}
+
+emptyScope = Scope M.empty M.empty M.empty
 
 compile :: AST -> Module
 compile ast = genMod $ compileAST ast
   where
-        compileAST (MathExpr expr) =  export "f" $ fun i32  $ ret $ genExpr expr
+        compileAST (MathExpr expr) =  export "f" $ fun i32  $ ret $ genExpr emptyScope expr
         compileAST (FunDecl fn) =  genFn fn
-        compileCond (BoolLit True) =  i32c 1
-        compileCond (BoolLit False) =  i32c 0
-        compileCond (BoolCompare  left op right) =  do
-          let left' = genExpr left
-              right' = genExpr right
+        compileCond _ (BoolLit True) =  i32c 1
+        compileCond _ (BoolLit False) =  i32c 0
+        compileCond scope (BoolCompare  left op right) =  do
+          let left' = genExpr scope left
+              right' = genExpr scope right
           case op of
             Eq -> eq left' right'
             Leq -> le_s left' right'
             Geq -> ge_s left' right'
-        genExpr :: MathExpr -> GenFun (Proxy I32)
-        genExpr expr =
+        genExpr :: Scope -> MathExpr -> GenFun (Proxy I32)
+        genExpr scope expr =
           case expr of
             MathInt x -> i32c x
-            MathIf cond e1 e2 ->  if' (Inline $ Just I32) (compileCond cond) (genExpr e1) (genExpr e2)
+            MathIf cond e1 e2 -> 
+                if' (Inline $ Just I32) (compileCond scope cond) (genExpr scope e1) (genExpr scope e2)
             MathBinExpr op x y -> do
-               let x' = genExpr x
-                   y' = genExpr y
-               case op of
-                 Add -> x' `add` y'
-                 Sub -> x' `sub` y'
-                 Mul -> x' `mul` y'
-                 Div -> x' `div_u` y'
-        genFn (Fun name args body) = do
-          let body' = genExpr body          
-          export (TL.pack name) $ fun i32 $ ret body'
+                let x' = genExpr scope x
+                    y' = genExpr scope y
+                case op of
+                  Add -> x' `add` y'
+                  Sub -> x' `sub` y'
+                  Mul -> x' `mul` y'
+                  Div -> x' `div_u` y'
+        genFn (Fun name args body) =  do
+          export (TL.pack name) $ fun i32 $ do
+            args' <-  mapM (\ a ->   (a,) <$> param i32) args        -- args' <- mapM (\ n -> (n,) <$> param i32) args          
+            let  scope = emptyScope { scopeParams = M.fromList args' }
+                 body' = genExpr scope body
+            ret body'
                
                

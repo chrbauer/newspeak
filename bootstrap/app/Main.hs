849ed903b5ea746cf7845fd09d694d6a809c1c18
Christoph Bauer <mail@christoph-bauer.net>
2023-06-03 10:22:04 +0200

    interactive loop

--- bootstrap/app/Main.hs
+++ bootstrap/app/Main.hs
@@ -10,10 +10,29 @@ import Language.Wasm.Interpreter
 import Language.Wasm.Validate
 import qualified Data.ByteString.Lazy as BS
 import Text.Megaparsec.Error
+import Data.Text (Text)
+import qualified Data.Text as T
+
+import System.Console.Haskeline
+
+import Control.Monad.IO.Class
 
 main :: IO ()
-main = do
-  case parse "if 6 >= 7 then 1 else 2" of
+main = runInputT defaultSettings loop
+   where
+       loop :: InputT IO ()
+       loop = do
+           minput <- getInputLine "% "
+           case minput of
+               Nothing -> return ()
+               Just "quit" -> return ()
+               Just input -> do outputStrLn $ "Input was: " ++ input
+                                liftIO $ run (T.pack input)
+                                loop
+
+run :: Text -> IO ()
+run line = do
+  case parse line of
     Left err -> putStrLn $ errorBundlePretty err
     Right ast -> do
       let m = compile ast

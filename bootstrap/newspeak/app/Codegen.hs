5f996342c9d76eeac3668f34f7a0aa11cfacabc3
Christoph Bauer <mail@christoph-bauer.net>
2025-07-13 10:49:45 +0200

    corrections

--- bootstrap/newspeak/app/Codegen.hs
+++ bootstrap/newspeak/app/Codegen.hs
@@ -1,16 +1,23 @@
-
-
 module Codegen (emitJS) where
 
 import AST
 import Data.List       (intersperse, intercalate)
 import qualified Data.Map as Map
+import Data.Char       (isAlphaNum)
+
+--------------------------------------------------------------------------------
+-- | Top-level entry
+--------------------------------------------------------------------------------
 
 emitJS :: Program -> String
 emitJS (Program bindings) =
      "const { store, fetch, update, int_print, int_gr, int_add } = require('./rts.js');\n\n"
   ++ Map.foldMapWithKey emitBinding bindings
   ++ "\nconsole.log(main());\n"
+
+--------------------------------------------------------------------------------
+-- | Function bindings
+--------------------------------------------------------------------------------
 
 emitBinding :: Var -> Binding -> String
 emitBinding name (Binding args body) =
@@ -21,122 +28,85 @@ emitBinding name (Binding args body) =
 emitArgs :: [Var] -> String
 emitArgs = concat . intersperse ", "
 
+--------------------------------------------------------------------------------
+-- | Line-wise emitter
+--------------------------------------------------------------------------------
+
 emitLines :: Exp -> [String]
 emitLines (Bind lp se rest) =
   case lp of
-    -- simple variable binder: as before
+    -- simple variable binder
     SVal (Var v) ->
-      [ "let " ++ v ++ " = " ++ emitSExp se ++ ";" ]
+      [ "let " ++ sanitizeVar v ++ " = " ++ emitSExp se ++ ";" ]
       ++ emitLines rest
 
-    -- constructor‐pattern binder: bind the raw node in tmp, then destructure
+    -- constructor pattern: allocate tmp, then destructure fields
     TagN tag fields ->
-      let tmp = "_pat"  -- you can pick any temp name
+      let tmp      = "_pat"
           bindNode = "const " ++ tmp ++ " = " ++ emitSExp se ++ ";"
-          varNames     = [ v | Var v <- fields ]
+          varNames = [ sanitizeVar v | Var v <- fields ]
           destruct = "const [" ++ intercalate ", " varNames ++ "] = " ++ tmp ++ ".fields;"
-      in [ bindNode, destruct ]
-         ++ emitLines rest
+      in  [bindNode, destruct]
+       ++ emitLines rest
 
-    -- nullary constructor pattern: no fields to extract
+    -- nullary constructor
     Tag0 tag ->
       let tmp = "_pat"
       in [ "const " ++ tmp ++ " = " ++ emitSExp se ++ ";" ]
-         ++ emitLines rest
+       ++ emitLines rest
 
-    -- literal pattern: just bind the value
+    -- literal pattern
     SVal (Literal n) ->
       [ "const _lit = " ++ show n ++ ";" ]
       ++ emitLines rest
 
-    -- empty‐tuple pattern: no fields
+    -- empty tuple
     EmptyTuple ->
       [ "const _pat = " ++ emitSExp se ++ ";" ]
       ++ emitLines rest
 
 emitLines (SExp se) =
   ["return " ++ emitSExp se ++ ";"]
-emitLines (Case val branches) = emitCase val branches
 
+emitLines (Case val branches) =
+  emitCase val branches
+
+--------------------------------------------------------------------------------
+-- | Case expression emitter
+--------------------------------------------------------------------------------
 
 emitCase :: Val -> [(CPat, Exp)] -> [String]
 emitCase val branches =
      ["switch (" ++ emitVal val ++ ") {"]
   ++ concatMap emitNominal branches
   ++ ["}"]
-  where
-    emitNominal (pat, expr) =
-      let
-        -- Build the `case` label and the list of pattern vars
-        (lbl, vs) = case pat of
-          TagNPat t vs'  -> ("case " ++ show t ++ ": {", vs')
-          Tag0Pat t      -> ("case " ++ show t ++ ":",     [])
-          LiteralPat n   -> ("case " ++ show n ++ ":",     [])
-          _              -> error "unexpected variable‐pattern"
+ where
+  emitNominal (pat, expr) =
+    let
+      -- case label and vars
+      (lbl, vs) = case pat of
+        TagNPat t vs' -> ("case " ++ show t ++ ": {", vs')
+        Tag0Pat t     -> ("case " ++ show t ++ ":",     [])
+        LiteralPat n  -> ("case " ++ show n ++ ":",     [])
+        _             -> error "unexpected pattern"
 
-        -- Destructure fields _first_, if any
-        extractLines
-          | null vs   = []
-          | otherwise = ["  const [" ++ intercalate ", " vs ++ "] = l2.fields;"]
-        
-        -- Body of the branch
-        bodyLines    = map ("  " ++) (emitLines expr)
+      -- destructure if needed
+      extractLines
+        | null vs   = []
+        | otherwise = ["  const [" ++ intercalate ", " (map sanitizeVar vs)
+                       ++ "] = l2.fields;"]
 
-        -- Closing lines
-        closingLines = ["  break;", "}"]
-      in
-        -- Concatenate the pieces:
-        [lbl]
-     ++ extractLines
-     ++ bodyLines
-     ++ closingLines
+      bodyLines    = map ("  " ++) (emitLines expr)
+      closingLines = ["  break;", "}"]
+    in
+      [lbl]
+   ++ extractLines
+   ++ bodyLines
+   ++ closingLines
 
-
-{-     
-
-emitCase :: Val -> [(CPat, Exp)] -> [String]
-emitCase val branches =
-      ["switch (" ++ emitVal val ++ ") {"]
-   ++ concatMap emitNominal nominalBranches
-   ++ emitDefault defaultBranch
-   ++ ["}"]
-  where
-    -- split the list once: at most one TagVarPat is allowed
-    (nominalBranches, defaultBranch) =
-        case span (not . isVarPat . fst) branches of
-          (ns, [])              -> (ns, Nothing)
-          (ns, [varBr])         -> (ns, Just varBr)
-          (_,  _:_:_) -> error "Multiple variable patterns in case"
-
-    isVarPat (TagVarPat _ _) = True
-    isVarPat _               = False
-
-    -- ordinary case labels
-    emitNominal (pat, expr) =
-      let (lbl, vs) = case pat of
-            TagNPat t vs'  -> ("case " ++ show t ++ ": {", vs')
-            Tag0Pat t      -> ("case " ++ show t ++ ":",     [])
-            LiteralPat n   -> ("case " ++ show n ++ ":",     [])
-            _              -> error "unexpected TagVarPat here"
-          body = map ("  " ++) (emitLines expr)
-          -- Destructure the `fields` array into the pattern variables:
-          extractLine
-            | null vs   = []
-            | otherwise = ["  const [" ++ concat (intersperse ", " vs) ++ "] = l2.fields;"]
-          tailLines =
-             extractLine
-          tailLines' = tailLines ++ ["  break;", "}"]          
-      in lbl : body ++ tailLines
-
-    -- default branch from TagVarPat (if present)
-    emitDefault Nothing = []
-    emitDefault (Just (TagVarPat var vs, expr)) =
-      ["default: {"]
-      ++ ["  const " ++ var ++ " = " ++ emitVal (SVal (Var var)) ++ ";"]
-      ++ ["  // TODO: extract fields " ++ show vs | not (null vs)]
-      ++ map ("  " ++) (emitLines expr)
-      ++ ["  break;", "}"]
-   -}   
+--------------------------------------------------------------------------------
+-- | Primitive emitters
+--------------------------------------------------------------------------------
 
 emitVal :: Val -> String
 emitVal (SVal sval)   = emitSVal sval
@@ -145,38 +115,44 @@ emitVal (TagN t _)    = t
 emitVal EmptyTuple    = "undefined"
 
 emitExp :: Exp -> String
-emitExp (SExp se)           = emitSExp se
-emitExp (Bind (SVal (Var v)) se rest)    =
-  let line = "let " ++ v ++ " = " ++ emitSExp se ++ ";"
+emitExp (SExp se)                       = emitSExp se
+emitExp (Bind (SVal (Var v)) se rest)   =
+  let line = "let " ++ sanitizeVar v ++ " = " ++ emitSExp se ++ ";"
   in line ++ "\n" ++ emitExp rest
-emitExp (Case val branches) =
-  "/* case inside parentheses not supported yet */"
-
+emitExp (Case _ _) =
+  "/* nested case in expression not supported yet */"
 
 emitSExp :: SExp -> String
-emitSExp (Unit val)         = emitVal val
+emitSExp (Unit val)          = emitVal val
 emitSExp (App (f:xs))        = emitSVal f ++ "(" ++ emitArgList xs ++ ")"
 emitSExp (Fetch p (Just i))  = "fetch("  ++ p ++ ", " ++ show i ++ ")"
-emitSExp (Fetch p Nothing)  = "fetch("  ++ p ++ ")"
-emitSExp (Update p val)     = "update(" ++ p ++ ", " ++ emitVal val ++ ")"
-emitSExp (Exp exp)          = "(" ++ emitExp exp ++ ")"  -- ← parentheses
-emitSExp (Store val) = case val of
-  SVal sval            ->
-    "store(" ++ emitSVal sval ++ ")"
-  Tag0 tag             ->
-    "store(\"" ++ tag ++ "\")"
-  TagN tag fields      ->
-    "store(\"" ++ tag ++ "\"," ++ emitFieldList fields ++ ")"
-  EmptyTuple           ->
-    "store()"
-  where
-    emitFieldList :: [SVal] -> String
-    emitFieldList = concat . intersperse ", " . map emitSVal
-
+emitSExp (Fetch p Nothing)   = "fetch("  ++ p ++ ")"
+emitSExp (Update p val)      = "update(" ++ p ++ ", " ++ emitVal val ++ ")"
+emitSExp (Exp exp)           = "(" ++ emitExp exp ++ ")"
+emitSExp (Store val)         = case val of
+  SVal sval          -> "store(" ++ emitSVal sval ++ ")"
+  Tag0 tag           -> "store(\"" ++ tag ++ "\")"
+  TagN tag fields    -> "store(\"" ++ tag ++ "\"," ++ emitFieldList fields ++ ")"
+  EmptyTuple         -> "store()"
+ where
+  emitFieldList :: [SVal] -> String
+  emitFieldList = concat . intersperse ", " . map emitSVal
 
 emitSVal :: SVal -> String
 emitSVal (Literal n) = show n
-emitSVal (Var     v) = v
+emitSVal (Var     v) = sanitizeVar v
 
 emitArgList :: [SVal] -> String
 emitArgList = concat . intersperse ", " . map emitSVal
+
+--------------------------------------------------------------------------------
+-- | Identifier sanitization
+--------------------------------------------------------------------------------
+
+sanitizeVar :: String -> String
+sanitizeVar = concatMap replace
+  where
+    replace '\'' = "_prime"
+    replace c
+      | isAlphaNum c = [c]
+      | otherwise    = ""
